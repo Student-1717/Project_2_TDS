fb3dca64ad1e481e347fd7317f9e79b1a6486cd1
Student-1717 <24f2005754@ds.study.iitm.ac.in>
2025-08-14 18:09:34 +0530

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,15 +1,21 @@
-
 import re
+import json
 from scraper import scrape_and_analyze
 from analyzer import analyze_data_files
 
 def process_request(question_path, file_dict):
-    with open(question_path, "r") as f:
+    with open(question_path, "r", encoding="utf-8") as f:
         question = f.read()
 
     url_match = re.search(r'https?://[^\s)]+', question)
     if url_match:
         url = url_match.group(0)
-        return scrape_and_analyze(url, question)
+        result = scrape_and_analyze(url, question)
+    else:
+        result = analyze_data_files(question, file_dict)
 
-    return analyze_data_files(question, file_dict)
+    # Guarantee JSON-serializable dict
+    if not isinstance(result, dict):
+        result = {"error": "Invalid return type"}
+
+    return json.dumps(result)
